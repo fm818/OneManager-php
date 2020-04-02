d3e1a8afbc4362c22801b62984d4521debec2c03
qkqpttgf <45693631+qkqpttgf@users.noreply.github.com>
2020-04-02 12:59:34 +0800

    Update heroku.php

--- platform/heroku.php
+++ platform/heroku.php
@@ -2,6 +2,7 @@
 
 function getpath()
 {
+	$_SERVER['firstacceptlanguage'] = strtolower($_SERVER['HTTP_ACCEPT_LANGUAGE']);
     $_SERVER['REMOTE_ADDR'] = $_SERVER['HTTP_X_FORWARDED_FOR'];
     $_SERVER['base_path'] = path_format(substr($_SERVER['SCRIPT_NAME'], 0, -10) . '/');
     $p = strpos($_SERVER['REQUEST_URI'],'?');
